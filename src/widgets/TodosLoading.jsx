a03dd1fc4bc90c700684dc7360d34b5fcdff2589
Marvin Magana <marvfadev@gmail.com>
2021-10-01 13:13:31 -0600

    Deploy 3

--- src/widgets/TodosLoading.jsx
+++ src/widgets/TodosLoading.jsx
@@ -4,7 +4,7 @@ import { BulletList } from "react-content-loader";
 const TodoLoader = () => (
   <BulletList
     style={{ width: "95%", margin: "0px auto" }}
-    viewBox="0 0 150 150"
+    viewBox="0 0 150 140"
     foregroundColor="#6d9886"
     backgroundColor="#7EB5A6"
   ></BulletList>
